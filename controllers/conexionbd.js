6bbdaf37fa27babc6c9f91edd11d10bd61afcf87
Juan Jose Hernandez Patiño <128816341+JuanJoseHernandezPatino@users.noreply.github.com>
2023-06-23 12:08:02 -0500

    conexion sql server
    se realizo la conexion sql server de forma exitosa

--- controllers/conexionbd.js
+++ controllers/conexionbd.js
@@ -1,14 +1,21 @@
 const sql = require('mssql')
 
+let confi = {
+    user: "calipso",
+    password: "juanjo2023...",
+    server: "localhost",
+    database: "musicProject",
+    options: {
+        encrypt: true,
+        trustServerCertificate: true,
+    }
+    //'Server=,1433;Database=musicProject;User Id=sa;Password=juanjo2023...;Encrypt=true'
+}
+
 async function getConexion() {
     try {
-        // make sure that any items are correctly URL encoded in the connection string
-        await sql.connect('Server=(localdb)\\calipsoServer,1433;Database=musicProject;User Id=calipsoLogin;Password=juanjo2023...;Encrypt=true')
-
-            ('Server=localhost,1433;Database=database;User Id=username;Password=password;Encrypt=true')
-
-        const result = await sql.query`select * from Usuarios`
-        console.dir(result)
+        let pool = await sql.connect(confi);
+        return pool;
     } catch (err) {
         console.log(err)
     }
